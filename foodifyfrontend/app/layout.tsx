b4000630c8852da8aee4b51b52b9af8647c17095
tim zimmermann laptop <tim.zimmermann2411@gmail.com>
2025-06-26 10:32:55 +0200

    init commit

---
+++ foodifyfrontend/app/layout.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hallo 👋</h1>
+      <p>Willkommen in deinem Next.js-Projekt!</p>
+    </main>
+  );
+}
